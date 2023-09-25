2975015b18ffffc8792ddac58676e8217c942348
chenxing <mitrecx@163.com>
2023-09-25 21:59:32 +0800

    添加列表渲染功能

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <script setup>
+import { getChildState } from 'element-plus/es/components/tree/src/model/node';
 import Edit from './components/Edit.vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios';
 
 // TODO: 列表渲染
-
+// 思路: 声明响应式 list -> 调用接口获取数据 
+// -> 后端数据赋值给 list -> 绑定到 table 组件
+const list = ref([])
+const getList = async () => {
+  // 接口调用
+  const res = await axios.get('/list')
+  list.value = res.data
+}
+onMounted(() => getList())
 
 // TODO: 删除功能
 
@@ -13,11 +24,7 @@ import Edit from './components/Edit.vue'
 
 <template>
   <div class="app">
-    <el-table :data="[{
-      id: 1,
-      name: 'jack',
-      place: 'none'
-    }]">
+    <el-table :data="list">
       <el-table-column label="ID" prop="id"></el-table-column>
       <el-table-column label="姓名" prop="name" width="150"></el-table-column>
       <el-table-column label="籍贯" prop="place"></el-table-column>
