f808993959457826bd455595835daae55160a53b
chenxing <mitrecx@163.com>
2023-09-25 22:15:45 +0800

    Implement delete button

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,12 @@ const getList = async () => {
 onMounted(() => getList())
 
 // TODO: 删除功能
-
+// 思路: 获取当前行 id -> 通过 id 调用删除接口 -> 更新列表
+const onDelete = async(id) => {
+  console.log(id)
+  await axios.delete(`/del/${id}`)
+  getList()
+}
 
 // TODO: 编辑功能
 
@@ -29,9 +34,9 @@ onMounted(() => getList())
       <el-table-column label="姓名" prop="name" width="150"></el-table-column>
       <el-table-column label="籍贯" prop="place"></el-table-column>
       <el-table-column label="操作" width="150">
-        <template #default>
+        <template #default="{ row }">
           <el-button type="primary" link>编辑</el-button>
-          <el-button type="danger" link>删除</el-button>
+          <el-button type="danger" @click="onDelete(row.id)" link>删除</el-button>
         </template>
       </el-table-column>
     </el-table>
