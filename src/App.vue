97e8de0cc613b83f5b8117b1ec6b7177b7cac4d8
chenxing <mitrecx@163.com>
2023-09-25 22:58:06 +0800

    Implement edition

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,17 @@ const onDelete = async(id) => {
 }
 
 // TODO: 编辑功能
-
+// 思路: 打开弹框 -> 回填数据 -> 更新数据
+// 1. 打开弹框(获取子组件实例, 调用它的方法修改)
+const editRef = ref(null)
+// const onEdit = () => {
+//   editRef.value.open()
+// }
+// 2. 回填数据(要么调用详情接口 / 要么用当前行的静态数据)
+// 这里使用 当前行的静态数据
+const onEdit = (row) => {
+  editRef.value.open(row)
+}
 </script>
 
 <template>
@@ -35,13 +45,13 @@ const onDelete = async(id) => {
       <el-table-column label="籍贯" prop="place"></el-table-column>
       <el-table-column label="操作" width="150">
         <template #default="{ row }">
-          <el-button type="primary" link>编辑</el-button>
+          <el-button type="primary" @click="onEdit(row)" link>编辑</el-button>
           <el-button type="danger" @click="onDelete(row.id)" link>删除</el-button>
         </template>
       </el-table-column>
     </el-table>
   </div>
-  <Edit />
+  <Edit ref = "editRef" @on-update="getList"/>
 </template>
 
 <style scoped>
--- src/components/Edit.vue
+++ src/components/Edit.vue
@@ -1,26 +1,60 @@
 <script setup>
 // TODO: 编辑
 import { ref } from 'vue'
+import axios from 'axios';
 // 弹框开关
 const dialogVisible = ref(false)
 
+// 接收数据
+const form = ref({
+  id: '',
+  name: '',
+  place: ''
+})
 
+const open = (row) => {
+  console.log(row);
+
+  form.value.id = row.id
+  form.value.name = row.name
+  form.value.place = row.place
+
+  dialogVisible.value = true
+}
+defineExpose({
+  open
+})
+
+// 更新
+const emit = defineEmits(['on-update'])
+const onUpdate = async () => {
+  // 1. 收集表单数据, 调用接口
+  await axios.patch(`/edit/${form.value.id}`, {
+    name: form.value.name,
+    place: form.value.place,
+  })
+  // 2. 关闭弹框
+  dialogVisible.value = false
+  // 3. 通知父组件更新列表(自定义事件)
+  emit('on-update')  
+
+}
 </script>
 
 <template>
   <el-dialog v-model="dialogVisible" title="编辑" width="400px">
     <el-form label-width="50px">
       <el-form-item label="姓名">
-        <el-input placeholder="请输入姓名" />
+        <el-input placeholder="请输入姓名" v-model="form.name" />
       </el-form-item>
       <el-form-item label="籍贯">
-        <el-input placeholder="请输入籍贯" />
+        <el-input placeholder="请输入籍贯" v-model="form.place" />
       </el-form-item>
     </el-form>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">取消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确认</el-button>
+        <el-button type="primary" @click="onUpdate">确认</el-button>
       </span>
     </template>
   </el-dialog>
